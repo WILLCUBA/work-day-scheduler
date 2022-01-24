59fe6bd73ead24e46eaaca1fe39b5c21f8d22fb6
Will <wilito1210@gmail.com>
2022-01-24 00:47:30 -0500

    bg for color tasks feature done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 //date
+var date = new Date;
 //update the date and the text content
 function updateTime() {
-    /// Increment serverTime by 1 second and update the html for '#time'
-    var date = moment().format('MMMM Do YYYY, h:mm:ss a');
+    var date = new Date;
+    date = date.toString().slice(0,24)
     $('#currentDay').html(date);
 }
 // call update every second
@@ -10,5 +11,23 @@ function updateTime() {
 $(function() {
     updateTime();
     setInterval(updateTime, 1000);
+    bgColorTasks()
 });
 
+// TIMEBLOCK CODE
+
+var hours = parseInt(date.getHours())
+var day = date.getDay()
+
+
+var bgColorTasks = function() {
+    $(".task-container").each(function() {
+        if (parseInt(this.parentNode.id) === hours) {
+            $(this).addClass("present")
+        } else if (parseInt(this.parentNode.id) < hours) {
+            $(this).addClass("past")
+        } else if (parseInt(this.parentNode.id) > hours) {
+            $(this).addClass("future")
+        }
+    })
+}
